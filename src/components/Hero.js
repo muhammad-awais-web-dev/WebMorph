f0b93ef06545867fabca6c520054a1fe951ad0d0
Muhammad Awais <awaisrafique4929@gmail.com>
2025-08-14 16:34:14 +0500

    fixed pdf pickng and completed hero section

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -1,6 +1,14 @@
 import { Link } from "react-router-dom"
 import Profile from '../resourses/Profile.webp' 
 
+function Overview(props){
+    return(
+    <div className="overview">
+        <h3 >{props.value}</h3>
+        <p >{props.name}</p>
+    </div>
+    )
+}
 
 function HeroSection(){
     return(
@@ -13,16 +21,23 @@ function HeroSection(){
                     <h2>Web Developer & UX/UI Designer</h2>
                     <p>Creating responsive, user-friendly websites and interfaces with React.js, WordPress, and modern design tools. Specializing in clean code and beautiful user experiences.</p>
                     <div className="buttonContainer">
-                        <Link className="button" to={"/projects"} >View my work</Link>
-                        <a className="button" href="..\resourses\muhammad-awais.pdf" download>Download CV</a>
+                        <Link className="button" to={"/projects"} >View my work  <i className="fa-solid fa-briefcase"></i></Link>
+                        <a className="button" href="/assets/muhammad-awais.pdf" download>Download CV  <i className="fa-solid fa-download"></i></a>
                     </div>
+                    <p><i className="fa-solid fa-code"></i>   React.js   <i className="fa-solid fa-palette"></i>   UI/UX Design  <i className="fa-solid fa-mobile"></i>   Responsive   </p>
                 </div>
             </div>
         </div>
         <div className="subSections">
             <div id="heroCard">
-                <img src={Profile} alt="  " />
-                <h1>Hi</h1>
+                <img className="profilePic" src={Profile} alt="  " />
+                <h2>Muhammad Awais</h2>
+                <p>Front-End Developer & Ui/Ux Designer</p>
+                <div>
+                    <Overview value='32+' name='Projects' />
+                    <Overview value='1+' name='Years' />
+                    <Overview value='7+' name='Clients' />
+                </div>
             </div>
         </div>
     </section>
