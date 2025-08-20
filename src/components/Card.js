90b2ad366379d0c738b43cdf22592d74afe2d651
Muhammad Awais <awaisrafique4929@gmail.com>
2025-08-20 13:16:39 +0500

    added more effects to project cards

--- src/components/Card.js
+++ src/components/Card.js
@@ -2,16 +2,32 @@ import './Card.css';
 
 export default function Card(props) {
   return (
-    <div className='Card'>
-        <img className='CardImg' src={props.image} alt={props.title} />
-        <div className="info">
+    <>
+      {props.link && props.link.length > 0 ? (
+        <a href={props.link}>
+          <div className='Card'>
+            <img className='CardImg' src={props.image} alt={props.title} />
+            {props.featured ? <span className='badge'>Featured</span> : ""}
+              <h3>{props.title}</h3>
+              <ul>
+              {props.skills.map((skill, index) => (
+                <li key={index}>{skill}</li>
+              ))}
+              </ul>
+          </div>
+        </a>
+      ) : (
+        <div className='Card'>
+          <img className='CardImg' src={props.image} alt={props.title} />
+          {props.featured ? <span className='badge'>Featured</span> : ""}
             <h3>{props.title}</h3>
             <ul>
             {props.skills.map((skill, index) => (
-                <li key={index}>{skill}</li>
+              <li key={index}>{skill}</li>
             ))}
             </ul>
         </div>
-    </div>
+      )}
+    </>
   )
 }
--- src/components/ProjectsHome.js
+++ src/components/ProjectsHome.js
@@ -6,8 +6,8 @@ import './ProjectsHome.css';
 export default function ProjectsHome() {
   return (
     <div className="ProjectList">
-      <Card image={RangoRawiyat} title="Rang-o-Riwayat" skills={[ "Wordpress", "Elementor","Ecommerce" ]} />
-      <Card image={Delicacy} title="Delicacy" skills={[ "Wordpress", "Elementor","Ecommerce" ]} />
+      <Card image={RangoRawiyat} title="Rang-o-Riwayat" skills={[ "Wordpress", "Elementor","Ecommerce" ]} featured={true} link="https://rangoriwayat.com/" />
+      <Card image={Delicacy} title="Delicacy" skills={[ "Wordpress", "Elementor","Ecommerce" ]} featured={false} link="" />
     </div>
   )
 }
