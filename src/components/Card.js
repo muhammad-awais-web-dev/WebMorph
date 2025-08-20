bdbaf0b9536043906eee1a17f621df54e6dd95d3
Muhammad Awais <awaisrafique4929@gmail.com>
2025-08-20 16:07:29 +0500

    /

--- src/components/Card.js
+++ src/components/Card.js
@@ -2,21 +2,6 @@ import './Card.css';
 
 export default function Card(props) {
   return (
-    <>
-      {props.link && props.link.length > 0 ? (
-        <a href={props.link} target="_blank" rel="noopener noreferrer">
-          <div className='Card'>
-            <img className='CardImg' src={props.image} alt={props.title} />
-            {props.featured ? <span className='badge'>Featured</span> : ""}
-              <h3>{props.title}</h3>
-              <ul>
-              {props.skills.map((skill, index) => (
-                <li key={index}>{skill}</li>
-              ))}
-              </ul>
-          </div>
-        </a>
-      ) : (
         <div className='Card'>
           <img className='CardImg' src={props.image} alt={props.title} />
           {props.featured ? <span className='badge'>Featured</span> : ""}
@@ -27,8 +12,7 @@ export default function Card(props) {
             ))}
             </ul>
             <p>{props.desc}</p>
+            <a style={{textAlign:"center", width:"fit-content"}} href={props.link} className='button'>View Project</a>
         </div>
-      )}
-    </>
   )
 }
